b21f9edc32a23e98f5a5752551473d918b168f70
Fernando Piderit <fgpiderit@gmail.com>
2026-03-20 14:48:41 -0300

    Cambios en los nombres de las variables para que sean ASCII

--- R/expandir_eusilc.R
+++ R/expandir_eusilc.R
@@ -1,3 +1,11 @@
+#' Title
+#'
+#' @param .datos descripcion
+#' @param .base descripcion
+#' @param ... descripcion
+#'
+#' @returns valor
+#' @export
 expandir_eusilc <- function(
     .datos,
     #.tipo = c("cross", "long"),
@@ -17,21 +25,21 @@ expandir_eusilc <- function(
 
   if (.base == "P") {
     if (any(!(c("PB010", "PB020") %in% variables))) {
-      rlang::abort("Falta `PB010` (año) o `PB020` (país).")
+      rlang::abort("Falta `PB010` o `PB020` (pais).")
     }
     if (!(tipos_var["PB010"] %in% c("numeric", "integer"))) {
-      rlang::abort("`PB010` (año) debe ser numérica.")
+      rlang::abort("`PB010` debe ser numerica.")
     }
 
-    año <- unique(.datos$PB010)
+    anno <- unique(.datos$PB010)
     pais <- unique(.datos$PB020)
-    chequear_pais_año(pais, año)
-    chequear_variables(variables, tipos_var, necesarias_p[[as.character(año)]])
+    chequear_pais_anno(pais, anno)
+    chequear_variables(variables, tipos_var, necesarias_p[[as.character(anno)]])
 
     .datos <- construir_variables_p(.datos, .pais)
   } else {
     #TODO
-    rlang::warn("Todavía no preparé nada para las bases H")
+    rlang::warn("Todavia no prepare nada para las bases H")
   }
   #if (.base == "H" & is.null(.individuos)) {
   #  mensaje <- paste0("Para expandir la base tipo `H` se debe proveer `.individuos`.\n",
@@ -43,6 +51,7 @@ expandir_eusilc <- function(
   return(.datos)
 }
 
+# Auxiliares -----------------------------------------------------------------
 chequear_variables <- function(.variables, .tipos, .necesarias) {
   faltan <- .necesarias[!(.necesarias %in% .variables)]
   if (length(faltan) != 0) {
@@ -54,33 +63,33 @@ chequear_variables <- function(.variables, .tipos, .necesarias) {
   no_numericas <- .necesarias[!(.tipos[.necesarias] %in% c("numeric", "integer"))]
   no_numericas <- no_numericas[no_numericas != "PB020" & no_numericas != "HB020"]
   if (length(no_numericas) != 0) {
-    mensaje <- paste0("Las siguientes variables requeridas deberían ser numéricas y no lo son: `",
+    mensaje <- paste0("Las siguientes variables requeridas deberian ser numericas y no lo son: `",
                       paste0(no_numericas, collapse = "`, `"), "`")
     rlang::abort(mensaje)
   }
 }
 
-chequear_pais_año <- function(.pais, .año) {
+chequear_pais_anno <- function(.pais, .anno) {
   if (length(.pais) > 1) {
-    rlang::abort("Por ahora sólo se admiten bases de un único país.")
+    rlang::abort("Por ahora solo se admiten bases de un unico pais.")
   }
-  if (length(.año) > 1) {
-    rlang::abort("Por ahora sólo se admiten bases de un único año.")
+  if (length(.anno) > 1) {
+    rlang::abort("Por ahora solo se admiten bases de un unico anno.")
   }
   if (!(.pais %in% paises_admitidos)) {
-    mensaje <- paste0("La base debe ser de uno de los siguientes países: `",
+    mensaje <- paste0("La base debe ser de uno de los siguientes paises: `",
                       paste0(paises_admitidos, collapse = "`, `"),
                       ", no `", .pais, "`.")
     rlang::abort(mensaje)
   }
-  if (.año < 2021) {
-    rlang::abort("Por ahora sólo se admiten bases posteriores a 2021.")
+  if (.anno < 2021) {
+    rlang::abort("Por ahora solo se admiten bases posteriores a 2021.")
   }
 }
 
 construir_variables_p <- function(.datos, .pais, ...) {
   .datos <- .datos |>
-    mutate(
+    dplyr::mutate(
       # Bloque I
       pi01 = PB010,
       pi02 = PB020,
