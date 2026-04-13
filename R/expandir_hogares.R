3d4092cb96f76c637dd49f5a86f384d9f5d4f635
Fernando Piderit <fgpiderit@gmail.com>
2026-04-13 16:38:28 -0300

    Reorganización de los archivos .R

--- R/expandir_hogares.R
+++ R/expandir_hogares.R
@@ -90,3 +90,72 @@ expandir_hogares <- function(
 
   return(datos)
 }
+
+# ============================================================================
+#' Agrega variables de ingreso de la base P de la EU-SILC a nivel hogar.
+#'
+#' @param .personas Conjunto P de la EU-SILC expandido con [construir_vbles_p()] y, opcionalmente, con [construir_vbles_p_lmh()].
+#'
+#' @returns Conjunto de datos con ingresos individuales agregados a nivel hogar.
+agregar_personas <- function(.personas) {
+  personas <- .personas |>
+    dplyr::summarise(
+      # Bloque Y -----------------------
+      dplyr::across(c(dplyr::any_of(c("py01", "py02", "py03")), py04:py13), sum),
+      # Bloque P -----------------------
+      dplyr::across(c(dplyr::any_of(c("py01", "py02", "py03")), py04:py13), \(y) sum(y != 0), .names = "x{.col}"),
+      .by = c(pi01, pi02, pi04)
+    )
+  personas <- personas |>
+    dplyr::rename_with(.cols = dplyr::starts_with("xpy"), .fn = \(n) sub("xpy", "hp", n))
+
+  # ------------------------------------------
+  return(personas)
+}
+
+# ============================================================================
+#' Construye variables en la base H de la EU-SILC.
+#'
+#' @param .datos Conjunto H de la EU-SILC.
+#' @param ... ...
+#'
+#' @returns Conjunto H de la EU-SILC con variables adicionales.
+construir_vbles_h <- function(
+    .datos,
+    ...
+) {
+  hogares <- .datos |>
+    dplyr::mutate(
+      # Bloque I -----------------------
+      hi01 = HB010,
+      hi02 = HB020,
+      hi04 = HB030,
+      # Bloque D -----------------------
+      hd01 = HX040,
+      hd02a = "A definir",
+      hd02b = "A definir",
+      # Bloque L -----------------------
+      # Bloque Y -----------------------
+      hy14 = HY040N + HY090N,
+      hy15 = HY050N + HY060N + HY070N,
+      hy16 = HY080N + HY110N,
+      hy17 = hy14 + hy16,
+      hy18 = py08 + py09 + hy15,
+      hy19 = py10 + hy18,
+      hy20 = py12 + hy14 + hy15 + hy16,
+      hy21 = py13 + hy14 + hy15 + hy16,
+      dplyr::across(
+        .cols = c(dplyr::any_of(c("py01", "py02", "py03")), py04:py13, hy14:hy21),
+        .fns = \(y) y / 12, .names = "{.col}m"
+      ),
+      dplyr::across(
+        .cols = c(dplyr::any_of(c("py01", "py02", "py03")), py04:py13, hy14:hy21),
+        .fns = \(y) y / hd01, .names = "{.col}pc"
+      ),
+      hyxxq = "py01 a hy21 / PPA correspondiente",
+      .keep = "all"
+    )
+
+  # ------------------------------------------
+  return(hogares)
+}
