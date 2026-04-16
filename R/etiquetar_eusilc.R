ef4fa7ae38f41d5100895b2b5d567028011d7297
Fernando Piderit <fgpiderit@gmail.com>
2026-04-16 15:38:48 -0300

    Exportar etiquetar_eusilc()

--- R/etiquetar_eusilc.R
+++ R/etiquetar_eusilc.R
@@ -4,6 +4,7 @@
 #' @param ... ...
 #'
 #' @returns datos etiquetados
+#' @export
 etiquetar_eusilc <- function(.datos, ...) {
   .datos <- labelled::set_variable_labels(.datos, .labels = etq_variables, .strict = FALSE)
   .datos <- labelled::set_value_labels(.datos, .labels = etq_valores, .strict = FALSE)
